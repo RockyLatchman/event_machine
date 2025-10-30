18ca0716c09538bb6975bdd3a7139f64e0c0cfba
RockyLatchman <r.latchman3624@gmail.com>
2025-10-30 19:32:29 +0700

    Header and footer files

---
+++ src/views/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+  <html lang="en">
+      <head>
+          <title>Event Machine</title>
+          <link rel="stylesheet" href="localhost/public/css/main.css">
+          <script src="localhost/public/js/main.js"></script>
+      </head>
+      <body>
