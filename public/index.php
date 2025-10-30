6638cc8a47484ce729d6db1cc3ce95f61a607285
RockyLatchman <r.latchman3624@gmail.com>
2025-10-30 19:23:56 +0700

    Added additional files

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
 
-$name = 'Rocky';
-echo "its me {$name}";
+$page = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($page) {
+    case '/event_machine/':
+        require '../src/views/user/index.php';
+        break;
+
+}
