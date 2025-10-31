0ba2e86a54e751e85bdacd7556a3c499ad3a64d2
RockyLatchman <r.latchman3624@gmail.com>
2025-10-31 19:26:47 +0700

    CSS for homepage

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 $page = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
+$method = $_SERVER['REQUEST_METHOD'];
 require '../src/views/header.php';
 
 switch ($page) {
--- src/views/header.php
+++ src/views/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
   <html lang="en">
-      <head>
-          <title>Event Machine</title>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-          <link rel="stylesheet" href="localhost/public/css/main.css">
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/htmx/2.0.7/htmx.min.js" integrity="sha512-IisGoumHahmfNIhP4wUV3OhgQZaaDBuD6IG4XlyjT77IUkwreZL3T3afO4xXuDanSalZ57Un+UlAbarQjNZCTQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-          <script src="localhost/public/js/main.js"></script>
-      </head>
-      <body>
+    <head>
+        <title>Event Machine</title>
+        <link rel="stylesheet" href="https://use.typekit.net/jhj8lgl.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/htmx/2.0.7/htmx.min.js" integrity="sha512-IisGoumHahmfNIhP4wUV3OhgQZaaDBuD6IG4XlyjT77IUkwreZL3T3afO4xXuDanSalZ57Un+UlAbarQjNZCTQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <link rel="stylesheet" href="http://localhost/event_machine/public/css/main.css">
+        <script src="http://localhost/event_machine/public/js/main.js"></script>
+    </head>
+    <body>
