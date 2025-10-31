4efd90734e7212d6d4e9e66f9a65ac70b8dc8cca
RockyLatchman <r.latchman3624@gmail.com>
2025-10-31 08:57:28 +0700

    Event controller method stubs

---
+++ src/controllers/EventController.php
@@ -0,0 +1,32 @@
+<?php
+
+require '../models/Event.php';
+
+class EventController {
+    public function add_event() {
+
+    }
+    public function fetch_event($event_id) {
+
+    }
+
+    public function cancel_event() {
+        //The creator of the even can cancel the event
+    }
+
+    public function fetch_all_events() {
+        //Get all events
+    }
+
+    public function filter_events() {
+       //filter by user id, date, location and category
+    }
+
+    public function fetch_user_events(){
+        //filter_events() by user id and fetch_all_events()
+    }
+
+    public function edit_event($event_id) {
+
+    }
+}
