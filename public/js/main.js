1566a637e10567752114a054524a2cf96002e08a
RockyLatchman <r.latchman3624@gmail.com>
2025-11-01 10:30:48 +0700

    sidePanel template literal, friendNames and friendImages constant variables

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,29 @@
 const sendMessageButton = document.querySelector('#send');
+const friendNames = document.querySelectorAll('.right-column h4');
+const friendImages = document.querySelectorAll('.left-column img');
+const currentUser = {
+  'image' : '',
+  'name' : ''
+}
+
+const sidePanel = `
+    <div class="side-panel">
+       <div id="close">X</div>
+       <form method="POST">
+         <h3>Message</h3>
+         <div id="message-box">
+           <ul>
+           <li><img src="/event_machine/public/images/profiles/${currentUser.image}" alt="Image of ${currentUser.name}">${currentUser.name}</li>
+           </ul>
+           <textarea name="message" placeholder="Hola...."></textarea>
+          </div>
+          <input type="submit" value="Send" id="send">
+       </form>
+    </div>
+`;
+
+
+
 
 function loadSidePanel() {
   /*when the user clicks send message button
